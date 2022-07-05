c2399f69699206ad68cf96c3e11088bdebb54a44
CeronMoa <56114106+ceron999@users.noreply.github.com>
2022-07-05 16:41:20 +0900

    220705/ceron/이동,공격,점프 UI 추가
    이동,공격,점프 UI 추가

---
+++ 220626 7daysProject/Assets/Scripts/UI/MoveUI.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class MoveUI : MonoBehaviour, IDragHandler, IPointerDownHandler, IPointerUpHandler
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnDrag(PointerEventData ped)
+    {
+        
+    }
+
+    public void OnPointerDown(PointerEventData ped)
+    {
+
+    }
+
+    public void OnPointerUp(PointerEventData ped)
+    {
+
+    }
+}
