09ed2aac8a62f8948aadc265418731471a45b8d4
CeronMoa <56114106+ceron999@users.noreply.github.com>
2022-07-06 18:24:42 +0900

    220706/ceron/이동, 공격 함수 구현
    1. 이동 UI 클릭할 경우 해당 방향을 바라보며 이동하도록 구현
    2. 공격 UI 클릭할 경우 1회 공격하도록 구현(아직 collider는 건드리지 않음)
    3. 대화 UI,Scene 생성

---
+++ 220626 7daysProject/Assets/Scripts/UI/ActionUI.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ActionUI : MonoBehaviour
+{
+    //Mir 오브젝트
+    public GameObject mir;
+    Rigidbody2D mirRigid;
+    SpriteRenderer mirSpriteRenderer;
+    Animator mirAnimator;
+
+    void Start()
+    {
+        mirRigid = mir.GetComponent<Rigidbody2D>();
+        mirSpriteRenderer = mir.GetComponent<SpriteRenderer>();
+        mirAnimator = mir.GetComponent<Animator>();
+    }
+
+    public void MirAttack()
+    {
+        mirAnimator.Play("MirAttack");
+    }
+
+}
